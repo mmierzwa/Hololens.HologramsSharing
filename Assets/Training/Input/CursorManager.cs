a71b9df015ab5cf245903d0ed0560194baa0a760
Marek Mierzwa <marek.mierzwa@solidbrain.com>
2018-10-02 14:58:03 +0200

    Initial commit

---
+++ Assets/Training/Input/CursorManager.cs
@@ -0,0 +1,61 @@
+﻿using HoloToolkit.Unity;
+using UnityEngine;
+
+namespace Academy.HoloToolkit.Unity
+{
+    /// <summary>
+    /// CursorManager class takes Cursor GameObjects.
+    /// One that is on Holograms and another off Holograms.
+    /// 1. Shows the appropriate Cursor when a Hologram is hit.
+    /// 2. Places the appropriate Cursor at the hit position.
+    /// 3. Matches the Cursor normal to the hit surface.
+    /// </summary>
+    public class CursorManager : Singleton<CursorManager>
+    {
+        [Tooltip("Drag the Cursor object to show when it hits a hologram.")]
+        public GameObject CursorOnHolograms;
+
+        [Tooltip("Drag the Cursor object to show when it does not hit a hologram.")]
+        public GameObject CursorOffHolograms;
+
+        [Tooltip("Distance, in meters, to offset the cursor from the collision point.")]
+        public float DistanceFromCollision = 0.01f;
+
+        void Awake()
+        {
+            if (CursorOnHolograms == null || CursorOffHolograms == null)
+            {
+                return;
+            }
+
+            // Hide the Cursors to begin with.
+            CursorOnHolograms.SetActive(false);
+            CursorOffHolograms.SetActive(false);
+        }
+
+        void LateUpdate()
+        {
+            if (TrainingGazeManager.Instance == null || CursorOnHolograms == null || CursorOffHolograms == null)
+            {
+                return;
+            }
+
+            if (TrainingGazeManager.Instance.Hit)
+            {
+                CursorOnHolograms.SetActive(true);
+                CursorOffHolograms.SetActive(false);
+            }
+            else
+            {
+                CursorOffHolograms.SetActive(true);
+                CursorOnHolograms.SetActive(false);
+            }
+
+            // Place the cursor at the calculated position.
+            this.gameObject.transform.position = TrainingGazeManager.Instance.Position + TrainingGazeManager.Instance.Normal * DistanceFromCollision;
+
+            // Orient the cursor to match the surface being gazed at.
+            gameObject.transform.up = TrainingGazeManager.Instance.Normal;
+        }
+    }
+}
